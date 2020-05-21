d6cfe3cd0bc8a428bf0c6dcd416de9f8e011d191
zxx <v-xingzh@microsoft.com>
2020-05-21 11:05:31 +0800

    mobile

--- src/Mobile.vue
+++ src/Mobile.vue
@@ -21,7 +21,7 @@
         interval: 40,
         currentStyle: '',
         enableHtml: false,
-        fullStyle: [
+       fullStyle: [
           `/*
 * Inspired by http://strml.net/
 * hello，我叫周星星
@@ -34,44 +34,55 @@
 }
 /* 白色背景太单调了，我们来点背景 */
 html {
-  color: rgb(222,222,222); background: rgb(0,43,54);
+  color: rgb(222,222,222);
+  background: rgb(0,43,54);
 }
 /* 文字离边框太近了 */
 .styleEditor {
   padding: .5em;
   border: 1px solid;
-  margin: .5em;
   overflow: auto;
-  width: 45vw; height: 90vh;
+  width: 90vw;
+  margin: 2.5vh 5vw;
+  height: 90vh;
+}
+/* 太高了 */
+.styleEditor {
+  height: 45vh;
 }
 /* 代码高亮 */
-.token.selector{ color: rgb(133,153,0); }
-.token.property{ color: rgb(187,137,0); }
-.token.punctuation{ color: yellow; }
-.token.function{ color: rgb(42,161,152); }
-
+.token.selector{
+  color: rgb(133,153,0);
+}
+.token.property{
+  color: rgb(187,137,0);
+}
+.token.punctuation{
+  color: yellow;
+}
+.token.function{
+  color: rgb(42,161,152);
+}
 /* 加点 3D 效果呗 */
 html{
   perspective: 1000px;
 }
 .styleEditor {
   position: fixed; left: 0; top: 0;
-  -webkit-transition: none;
-  transition: none;
-  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
-          transform: rotateY(10deg) translateZ(-100px) ;
+  transform: rotateX(-10deg) translateZ(-50px) ;
 }
-
 /* 接下来我给自己准备一个编辑器 */
 .resumeEditor{
-  position: fixed; right: 0; top: 0;
-  padding: .5em;  margin: .5em;
-  width: 48vw; height: 90vh;
+  position: fixed;
+  top: 50%; left: 0;
+  padding: .5em;  margin: 2.5vh;
+  width: 90vw; height: 45vh;
   border: 1px solid;
   background: white; color: #222;
   overflow: auto;
 }
 /* 好了，我开始写简历了 */
+
 
 
 `,
